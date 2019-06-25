80de18b9ae98d6f6104ec9c35e85cb2b1ab2ad0b
Valery Emelyanov <emelva@rambler.ru>
2019-06-25 22:32:37 +0400

    SQL scripts for meals

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,11 +1,28 @@
-DELETE FROM user_roles;
-DELETE FROM users;
+DELETE
+FROM user_roles;
+DELETE
+FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
-INSERT INTO users (name, email, password) VALUES
-  ('User', 'user@yandex.ru', 'password'),
-  ('Admin', 'admin@gmail.com', 'admin');
+INSERT INTO users (name, email, password
+)
+VALUES ('User', 'user@yandex.ru', 'password'),
+       ('Admin', 'admin@gmail.com', 'admin');
 
-INSERT INTO user_roles (role, user_id) VALUES
-  ('ROLE_USER', 100000),
-  ('ROLE_ADMIN', 100001);
+INSERT INTO user_roles (role, user_id
+)
+VALUES ('ROLE_USER', 100000),
+       ('ROLE_ADMIN', 100001);
+
+ALTER SEQUENCE meals_id_seq RESTART WITH 1;
+
+INSERT INTO meals(date_time, description, calories, user_id)
+VALUES ('2015-05-30 10:00', 'Завтрак', 500, 100000)
+     , ('2015-05-30', 'Обед', 1000, 100000)
+     , ('2015-05-30', 'Ужин', 500, 100000)
+     , ('2015-05-31', 'Завтрак', 1000, 100000)
+     , ('2015-05-31', 'Обед', 500, 100000)
+     , ('2015-05-31', 'Ужин', 510, 100000)
+     , ('2015-05-31', 'Завтрак для Админа', 900, 100001)
+     , ('2015-05-31', 'Обед для Админа', 600, 100001)
+     , ('2015-05-31', 'Ужин для Админа', 600, 100001);
