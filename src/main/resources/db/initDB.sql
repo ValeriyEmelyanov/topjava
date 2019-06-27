09b6a65051450bbc82d8b760e7e2edc604bd93c4
Valery Emelyanov <emelva@rambler.ru>
2019-06-27 23:34:19 +0400

    Fixing populateDB.sql

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -35,4 +35,4 @@ CREATE TABLE meals
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
 
-CREATE INDEX meals_date_time ON meals (date_time);
+CREATE UNIQUE INDEX meals_unique_user_datetime_idx ON meals (user_id, date_time);
